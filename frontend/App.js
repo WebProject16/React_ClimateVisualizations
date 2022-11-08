b02fe2826c863f27675b5fe1e4ad850d9dbdbc56
pinjakemppainen <99818104+pinjakemppainen@users.noreply.github.com>
2022-11-08 19:27:58 +0200

    päivitystä

---
+++ frontend/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar.js';
+import Header from './Components/Header.js';
+import Home from './Components/Home.js';
+import Contact from './Components/Contact.js';
+import Create from './Components/Create.js';
+import Login from './Components/Login.js';
+import Footer from './Components/Footer.js';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    
+      <Navbar />
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Create" element={<Create />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+      <Footer />
+      </>
+  );
+}
+
+export default App;
---
+++ frontend/Components/Login.js
@@ -0,0 +1,12 @@
+export default function Login() {
+    return (
+
+        <div>
+        <label for="usernametxt">User name:</label>
+        <input type="usernametxt" id="usernametxt" name="usernametxt"></input>
+
+        <label for="psw">Password:</label>
+        <input type="password" id="psw" name="psw"></input>
+        </div>
+    )
+}
