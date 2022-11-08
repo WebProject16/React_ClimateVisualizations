e3db354a749d6b95accba668e07e2dbc9fd6bf3d
ryksyy <miko.prykari@gmail.com>
2022-11-08 20:03:43 +0200

    fixed folder structure

--- frontend/Components/Create.js
+++
@@ -1,8 +0,0 @@
-export default function Create() {
-    return (
-
-        <div>
-        
-        </div>
-    )
-}
---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+       <div className="container">
+         <span>Helmi Laakkonen, Pinja Kemppainen, Miko Prykäri ja Lasse Suomela</span>
+        </div>
+        </footer>
+
+    );
+}
