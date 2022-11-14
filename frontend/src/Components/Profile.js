9ff6b556e93d2fc860594db19d3fc2e40130dd68
HelmiLaakkonen <helmi.laakkonen@gmail.com>
2022-11-14 11:17:01 +0200

    Added button and form for deleting user

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -1,6 +1,14 @@
 import {Delete} from '../API/request';
+import { useState,  useEffect } from 'react';
 
 export default function Profile() {
+
+    const [isDeleting, setIsDeleting] = useState(false);
+    useEffect(() => {
+      setIsDeleting(false)
+    }, [])
+    
+
     const DelProfile = async () => {
         await Delete ("/somePath", 
         (res) => {
@@ -25,18 +33,33 @@ export default function Profile() {
         </li>  
     )
     return (
-        <>
         <div>
-            <h2>
+            <div>
+                <h2>
                 Welcome user!
-            </h2>
-        </div>
+                </h2>
+            </div>
         <div className="list-group">
             {views}
         </div>
-        <div>
-            <button onClick={DelProfile} type="button" class="btn btn-outline-danger">Delete user</button>
+            {isDeleting ? (
+            <form onSubmit={DelProfile}>
+                <h3>Confirm deleting user</h3>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" className="form-control"/>
+                </div>
+                <div>
+                    <label htmlFor='password'>Password:</label>
+                    <input type="text" id="password" className="form-control"/>                
+                </div>
+                <button type="submit" className="btn btn-outline-danger">Delete user?</button>
+            </form>
+            ) : (   
+            <div className='pb-2 pt-4'>
+                 <button onClick={()=> setIsDeleting(true)} type="button" className="btn btn-outline-danger">Delete user</button>
+            </div>
+            ) }
         </div>
-        </>
     )
 }
