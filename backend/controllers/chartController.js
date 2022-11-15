a648677a6f37d5c611848955166673c508b62c28
pinjakemppainen <pinja.kemppainen02@gmail.com>
2022-11-15 20:10:19 +0200

    added endpoints from v3 to v6

--- backend/controllers/chartController.js
+++ backend/controllers/chartController.js
@@ -16,7 +16,87 @@ const v1 = (req, res) => {
         res.json({status:"success", data: data})
     })
 }
+const v3_annual = (req, res) => {
+
+    chartModel.getV3_annual((err, data) => {
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:"error", msg:"Error on fetching chart V3_annual data"})
+        }
+
+        if(!data){
+            return res.status(500).json({status:"error", msg:"No data found in V3_annual table"})
+        }
+
+        res.json({status:"success", data: data})
+    })
+}
+const v3_monthly = (req, res) => {
+
+    chartModel.getV3_monthly((err, data) => {
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:"error", msg:"Error on fetching chart V3_monthly data"})
+        }
+
+        if(!data){
+            return res.status(500).json({status:"error", msg:"No data found in V3_monthly table"})
+        }
+
+        res.json({status:"success", data: data})
+    })
+}
+const v4 = (req, res) => {
+
+    chartModel.getV4((err, data) => {
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:"error", msg:"Error on fetching chart V4 data"})
+        }
+
+        if(!data){
+            return res.status(500).json({status:"error", msg:"No data found in V4 table"})
+        }
+
+        res.json({status:"success", data: data})
+    })
+}
+const v5 = (req, res) => {
+
+    chartModel.getV5((err, data) => {
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:"error", msg:"Error on fetching chart V5 data"})
+        }
+
+        if(!data){
+            return res.status(500).json({status:"error", msg:"No data found in V5 table"})
+        }
+
+        res.json({status:"success", data: data})
+    })
+}
+const v6 = (req, res) => {
+
+    chartModel.getV6((err, data) => {
+        if(err){
+            console.log(err);
+            return res.status(500).json({status:"error", msg:"Error on fetching chart V6 data"})
+        }
+
+        if(!data){
+            return res.status(500).json({status:"error", msg:"No data found in V6 table"})
+        }
+
+        res.json({status:"success", data: data})
+    })
+}
 
 module.exports = {
-    v1
+    v1,
+    v3_annual,
+    v3_monthly,
+    v4,
+    v5,
+    v6
 }
--- backend/models/chartModel.js
+++ backend/models/chartModel.js
@@ -4,6 +4,26 @@ const chart = {
    
     getV1: (cb) => {
         db.query("SELECT * FROM v1_hadcrut_annual", cb)
+    },
+
+    getV3_annual: (cb) => {
+        db.query("SELECT * FROM v3_mauna_loa_co2_annual", cb)
+    },
+
+    getV3_monthly: (cb) => {
+        db.query("SELECT * FROM v3_mauna_loa_co2_monthly", cb)
+    },
+
+    getV4: (cb) => {
+        db.query("SELECT * FROM v4_antarctic_co2", cb)
+    },
+    
+    getV5: (cb) => {
+        db.query("SELECT * FROM v5_vostok_co2", cb)
+    },
+
+    getV6: (cb) => {
+        db.query("SELECT * FROM v6_ice_core", cb)
     }
 }
 
