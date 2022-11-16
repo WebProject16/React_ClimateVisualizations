7c5d885f4394798821c7020d6dd5e6b8c0154684
Lasse Suomela <suomelasse@gmail.com>
2022-11-16 12:56:08 +0200

    multiaxis chart testings

--- frontend/src/Components/visualizations/V1.js
+++ frontend/src/Components/visualizations/V1.js
@@ -1,74 +1,48 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, {useEffect, useState} from "react";
 import { Chart } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 import { Get } from "../../API/request";
 import "chartjs-adapter-luxon";
 
 export default function V1() {
-  let dataYear = [];
-  let dataMonth = [];
-  let dataV2 = [];
-  const chartRef = useRef();
-  const [dataSets, setDataSets] = useState([]);
-  const [options, setOptions] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [ isAnnual, setIsAnnual ] = useState(false);
+  
+  const [dataYear, setDataYear] = useState([]);
+  const [dataMonth, setDataMonth] = useState([]);
+
+  const [isAnnual, setIsAnnual] = useState(true);
 
   useEffect(() => {
-    console.log("get data")
     Get("/charts/v1", (res) => 
     {
       if(res.status === 200){
-          dataYear.push(res.data.dataYear)
-          console.log(dataYear)
-        }else{
-          console.log("Error: ", res)
-        }
+
+        setDataYear(res.data.dataYear)
+        setDataMonth(res.data.dataMonth)
+
+      }else{
+        console.log("Error: ", res)
+      }
     })
-        
-    SetInfo();
   }, [])
 
-  useEffect(() => {
-
-  }, [isLoading])
-
-  useEffect(() => {
-    console.log("data set changed")
-    setIsLoading(false);
-    if(chartRef.current){
-      chartRef.current.render();
-      chartRef.current.update();
-    }
-  }, [dataSets])
-
-
-
-  const ToggleV2 = () => {
-    console.log("datayear", dataYear)
-    console.log("data ")
-    console.log("set", dataSets)
-  }
-
-  const SetInfo = () => {
-    setDataSets({datasets: 
-      [
+    const data = {
+      datasets: [
         {
-          label: "Global temperature anomaly",
-          data: dataYear,
-          borderColor: "rgb(0, 0, 0)",
-          backgroundColor: "rgb(0, 0, 0)",
+          label: "Northern temperature anomaly",
+          data: isAnnual ? dataYear : dataMonth,
+          borderColor: "rgb(50, 80, 200)",
+          backgroundColor: "rgb(50, 80, 200)",
           parsing: {
             xAxisKey: "year",
-            yAxisKey: "global_anomaly",
+            yAxisKey: "northern_anomaly",
           },
           pointRadius: 1,
         },
         {
-          label:"Southern temperature anomaly",
-          data: dataYear,
-          borderColor: "rgb(0, 255, 0)",
-          backgroundColor: "rgb(0, 255, 0)",
+          label: "Southern temperature anomaly",
+          data: isAnnual ? dataYear : dataMonth,
+          borderColor: "rgb(230, 150, 15)",
+          backgroundColor: "rgb(230, 150, 15)",
           parsing: {
             xAxisKey: "year",
             yAxisKey: "southern_anomaly",
@@ -76,72 +50,50 @@ export default function V1() {
           pointRadius: 1,
         },
         {
-          label:"Northern temperature anomaly",
-          data: dataYear,
-          borderColor: "rgb(0, 0, 255)",
-          backgroundColor: "rgb(0, 0, 255)",
+          label: "Global temperature anomaly",
+          data: isAnnual ? dataYear : dataMonth,
+          borderColor: "rgb(50, 150, 100)",
+          backgroundColor: "rgb(50, 150, 100)",
           parsing: {
             xAxisKey: "year",
-            yAxisKey: "northern_anomaly",
+            yAxisKey: "global_anomaly",
           },
           pointRadius: 1,
         }
       ]
-    })
-    setOptions(
-      {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: true,
-            text: "Temperature anomalies",
-          },
-        },
-        scales: {
-            x: {
-              type: "time",
-              time:{
-                unit:"year",
-              }
-            },
-            yAxis: {
-              type: "linear",
-            },
-          },
-        }  
-      )
-      console.log(dataSets)
-  }
+    }
 
+    const options =
+    {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "Temperature anomalies " + (isAnnual ? "(annual)" : "(month)"),
+        },
+      },
+      scales: {
+        x: {
+          type: "time",
+          time:{
+            unit: isAnnual ? "year" : "month",
+          }
+        },
+        yAxis: {
+          type: "linear",
+        },
+      },
+    }
 
   return (
     <>
-      {isLoading ? (
-        <>
-          <p>Loading please wait</p>
-        </>
-      ) : (
-        <>
-          <div className="container-fluid">
-            <Line ref={chartRef} data={dataSets} options={options}/>
-            {/* {isAnnual ? (
-              <>
-                <button onClick={ToggleAnnual} className="btn btn-outline-primary mt-4">Show monthly data</button>
-              </>
-            ) : (
-              <>
-                <button onClick={ToggleAnnual} className="btn btn-outline-primary mt-4">Show annual data</button>
-              </>
-            )} */}
-          </div>
-          <div className="container-fluid">
-            <button className="btn btn-outline-primary mt-2" onClick={ToggleV2}>Toggle v2</button>
-          </div>
-        </>
-      )}
+      <div className="container-fluid">
+        <Line data={data} options={options} alt="Anomaly data chart"/>
+        <button onClick={() => setIsAnnual(!isAnnual)} className="btn btn-outline-primary mt-4">{isAnnual ? "Show monthly data" : "Show yearly data"}</button>
+      </div>
     </>
   );
 }
