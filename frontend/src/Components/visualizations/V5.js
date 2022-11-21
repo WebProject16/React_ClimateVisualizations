a8174cbbda189e714d2198daf539de6a8e7705a2
Lasse Suomela <suomelasse@gmail.com>
2022-11-21 14:51:26 +0200

    added correct sources

--- frontend/src/Components/visualizations/V5.js
+++ frontend/src/Components/visualizations/V5.js
@@ -82,8 +82,8 @@ export default function V5() {
           <h5 className="card-title">Data description</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <h6 className="card-subtitle mt-2 text-muted">Sources:</h6>
-          <a href="https://www.metoffice.gov.uk/hadobs/hadcrut5/" target="_blank" rel="noreferrer" className="card-link">Met Office Hadley Centre</a>
-          <a href="https://bolin.su.se/data/moberg-2012-nh-1?n=moberg-2005" target="_blank" rel="noreferrer" className="card-link">Stockholm University</a>
+          <a href="https://cdiac.ess-dive.lbl.gov/trends/co2/vostok.html" target="_blank" rel="noreferrer" className="card-link">Description</a>
+          <a href="https://cdiac.ess-dive.lbl.gov/ftp/trends/co2/vostok.icecore.co2" target="_blank" rel="noreferrer" className="card-link">Data source</a>
         </div>
       </div>
     </>
