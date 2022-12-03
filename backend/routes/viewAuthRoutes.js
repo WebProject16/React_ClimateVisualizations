b7edc693366cb3cfd828bc5f795324fd822999cf
Lasse Suomela <suomelasse@gmail.com>
2022-12-03 16:04:22 +0200

    separated viewRoutes to two files because view by url need to be accessed w/o authentication

--- backend/routes/viewAuthRoutes.js
+++ backend/routes/viewAuthRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const viewController = require("../controllers/viewController.js");
 
-router.get("/:url", viewController.fetchViewByUrl);
+router.get("/users/all", viewController.fetchUsersViews);
+router.post("/", viewController.createView);
+router.delete("/:url", viewController.deleteView);
 
 module.exports = router;
