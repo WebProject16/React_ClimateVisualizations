b5e1e2240f2ca8cb48e14c7c40a7ed326a3537bb
Lasse Suomela <suomelasse@gmail.com>
2022-12-06 20:20:47 +0200

    some styling changes to create page, added css to view selector to hide scrollbar and to set height,

--- frontend/src/Components/Create.js
+++ frontend/src/Components/Create.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Create() {
 
     const [viewData, setViewData] = useState([]);
-    const [isParallel, setIsParallel] = useState(false);
+    const [isParallel, setIsParallel] = useState(true);
     const [description, setDescription] = useState("");
     const [isSuccess, setIsSuccess] = useState(false);
     const [url, setUrl] = useState("");
@@ -39,7 +39,7 @@ export default function Create() {
                 <h3>Valitse visuaalisaatiot jotka halua näyttää</h3>
 
                 <div className="p-4">
-                    <select className="form-select" name="views" onClick={e => setViewData(viewData => [...viewData, e.target.value])} multiple>
+                    <select className="form-select viewSelector" name="views" onClick={e => setViewData(viewData => [...viewData, e.target.value])} multiple>
                         <option value="v1">1850-2022 lämpötilan poikkeamat</option>
                         <option value="v3">Mauna Loa sekä Law Dome hiilidioksidipitoisuudet</option>
                         <option value="v5">Ilmakehän hiilidioksidipitoisuudet</option>
@@ -58,16 +58,24 @@ export default function Create() {
 
                     <div>
                         <h4>Kaavioiden asettelu</h4>
-                        <div>
-                            <input type="checkbox" className="btn-check" name="isParallel" id="isSideBySideSw" />
-                            <label className="btn btn-outline-primary" htmlFor="isSideBySideSw" onClick={() => setIsParallel(!isParallel)}>{isParallel ? "Vierekkäin" : "Allekkain"}</label>
+
+                        <div className="row">
+                            <div className="col">
+                                <input type="radio" className="btn-check" name="isParallel" id="isParallelSw"  defaultChecked={isParallel}></input>
+                                <label className="btn btn-outline-dark" htmlFor="isParallelSw" onClick={() => setIsParallel(true)}>Vierekkäin</label>
+                            </div>
+
+                            <div className="col">
+                                <input type="radio" className="btn-check" name="isParallel" id="isNonParallelSw" defaultChecked={!isParallel}/>
+                                <label className="btn btn-outline-dark" htmlFor="isNonParallelSw" onClick={() => setIsParallel(false)}>Allekkain</label>
+                            </div>
                         </div>
-                        <button type="submit" className="btn btn-outline-primary mt-4">Tallenna</button>
+                        <button type="submit" className="btn btn-outline-primary mt-4">Luo näkymä</button>
 
                         {
                         isSuccess ?
-                        <div className="pt-4 mt-4">
-                            <Link className="btn btn-success p-3 text-decoration-none"to={"/view/" + url}>Linkki näkymään</Link>
+                        <div className="pt-4">
+                            <Link className="btn btn-success p-2 text-decoration-none"to={"/view/" + url}>Linkki näkymään</Link>
                         </div>
                         :""
                         }
